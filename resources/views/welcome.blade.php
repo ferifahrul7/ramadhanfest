09ff6bdec530993bd3df33008da478efedd288f6
ferifahrul7 <ferifahrul7@gmail.com>
2021-04-24 10:06:20 +0700

    membuat tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,230 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <title>Ramadhan Pekalongan Fest 2021</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/frontend/icon/apple-icon-57x57.png') }}">
+  <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('assets/frontend/icon/apple-icon-60x60.png') }}">
+  <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/frontend/icon/apple-icon-72x72.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/frontend/icon/apple-icon-76x76.png') }}">
+  <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/frontend/icon/apple-icon-114x114.png') }}">
+  <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/frontend/icon/apple-icon-120x120.png') }}">
+  <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/frontend/icon/apple-icon-144x144.png') }}">
+  <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/frontend/icon/apple-icon-152x152.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/frontend/icon/apple-icon-180x180.png') }}">
+  <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/frontend/icon/android-icon-192x192.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/frontend/icon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/frontend/icon/favicon-96x96.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/frontend/icon/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('assets/frontend/manifest.json') }}">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="{{ asset('assets/frontend/icon/ms-icon-144x144.png') }}">
+  <meta name="theme-color" content="#ffffff">
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Krub:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('assets/frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/frontend/vendor/icofont/icofont.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/frontend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/frontend/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/frontend/vendor/venobox/venobox.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/frontend/vendor/aos/aos.css') }}" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
 
-            .content {
-                text-align: center;
-            }
+  <!-- =======================================================
+  * Template Name: Bikin - v2.2.1
+  * Template URL: https://bootstrapmade.com/bikin-free-simple-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
 
-            .title {
-                font-size: 84px;
-            }
+<body>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+      <!-- <h1 class="logo mr-auto"><a href="index.html">Ramadhan Pekalongan Fest 2021</a></h1> -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="#home" class="logo mr-auto"><img src="{{ asset('assets/img/logo/logo.png') }}" alt="" class="img-fluid"></a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#about">Tentang</a></li>
+          <li><a href="#steps">Teknis</a></li>
 
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <a href="#" class="get-started-btn">Daftar Sekarang!</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container d-flex flex-column align-items-center justify-content-center" data-aos="fade-up">
+      <h1>Ramadhan Pekalongan Fest 2021</h1>
+      <h2>Event Ramadhan Terbesar Di Pekalongan</h2>
+      <img src="{{ asset('assets/frontend/img/hero-img.png') }}" class="img-fluid hero-img" alt="" data-aos="zoom-in" data-aos-delay="150">
+
+      <a href="#about" class="btn-get-started scrollto">Penasaran?</a>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row no-gutters">
+          <div class="content col-xl-5 d-flex align-items-stretch" data-aos="fade-right">
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+              <h3>Ramadhan Pekalongan Fest 2021</h3>
+              <p>
+                Event Ramadhan Terbesar Di Pekalongan
+              </p>
+              <a href="#aboutus" class="about-btn">About us <i class="bx bx-chevron-right"></i></a>
             </div>
+          </div>
+          <div class="col-xl-7 d-flex align-items-stretch" data-aos="fade-left">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                  <i class="bx bx-microphone"></i>
+                  <h4>Lomba Religi</h4>
+                  <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                  <i class="bx bx-cookie"></i>
+                  <h4>Festival Kuliner</h4>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+                  <i class="bx bx-laugh"></i>
+                  <h4>Ngabuburit</h4>
+                  <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+                  <i class="bx bx-group"></i>
+                  <h4>Expo</h4>
+                  <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
+                </div>
+              </div>
+            </div><!-- End .content-->
+          </div>
         </div>
-    </body>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Steps Section ======= -->
+    <section id="steps" class="steps">
+      <div class="container">
+
+        <div class="row no-gutters" data-aos="fade-up">
+
+          <div class="col-lg-3 col-md-6 content-item" data-aos="fade-up" data-aos-delay="100">
+            <span>01</span>
+            <h5>Daftar Melalui Website</h5>
+            <p>Daftarkan diri Anda melalui website ini dan dapatkan kode pendaftaran.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 content-item" data-aos="fade-up" data-aos-delay="200">
+            <span>02</span>
+            <h5>Persiapkan Kode Pendaftaran Anda</h5>
+            <p>Tunjukkan kode tersebut kepada Panitia ketika akan menghadiri event.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 content-item" data-aos="fade-up" data-aos-delay="300">
+            <span>03</span>
+            <h5>Patuhi Protokol kesehatan</h5>
+            <p>Tetap patuhi protokol kesehatan saat mengikuti event.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 content-item" data-aos="fade-up" data-aos-delay="100">
+            <span>04</span>
+            <h5>Keluar dengan Tertib</h5>
+            <p>Keluar dengan menunjukkan kembali kode pendaftaran kepada Panitia.</p>
+          </div>
+
+        </div>
+    </section><!-- End Steps Section -->
+
+
+    <!-- ======= questions Section ======= -->
+    <section id="testimonials" class="questions section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Info Lebih Lanjut</h2>
+          <p>Ikuti sosial media kami untuk mendapatkan informasi lebih lanjut.</p>
+        </div>
+        <div class="social-links text-center pt-3 pt-md-0">
+          <a href="https://instagram.com/ramadhanpekalonganfest" class="instagram"><i class="bx bxl-instagram"></i></a> Instagram 
+        </div>
+      </div>
+    </section><!-- End questions Section -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Powered By <strong><span>Ramadhan Pekalongan Fest</span></strong>.
+        </div>
+        <div class="credits" style="display:none">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bikin-free-simple-landing-page-template/ -->
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://wa.me/+6282250500588" class="whatsapp"><i class="bx bxl-whatsapp"></i></a> Info Booking Tenant (Faki)
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('assets/frontend/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/venobox/venobox.min.js') }}"></script>
+  <script src="{{ asset('assets/frontend/vendor/aos/aos.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('assets/frontend/js/main.js') }}"></script>
+
+</body>
+
 </html>
